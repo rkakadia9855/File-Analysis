46a3035be47f28015f65ff8cc0a6a8b2c2497dcf
rkakadia9855 <rudrakakadia9855@gmail.com>
2020-11-24 10:09:23 +0530

    files, allThreads and tracker moved to global scope outside main

--- Asst2.c
+++ Asst2.c
@@ -9,6 +9,10 @@
 #include <sys/types.h>
 #include <pthread.h>
 
+struct filenode files[100]; //stores the nodes of files/directories
+pthread_t allThreads[100]; //stores the handles of all threads
+int tracker = 0; //tracks the total number of files/directories and threads at the same time
+
 struct wordnode {
     char* name;
     int totalCount;
@@ -19,8 +23,9 @@ struct filenode{
     char* name;
     struct filenode *nextFile; 
     struct wordnode *headWord;
-    pthread_t thread;
+  //  pthread_t thread;
     int index;
+ //   int* trackerPtr;
 };
 
 void *grow(struct filenode smt[100], int* capacityPTR) {
@@ -53,14 +58,19 @@ void* handleDir(void* kmt) {
     char* name = funcArgs->name;
 
     printf("Handling: %s\n", name);
-    
-    if(strcmp(name, "smt"))
-        numLoop = 10000000;
-    else if(strcmp(name, "dmt"))
-        numLoop = 10000;
-    for(int i = 0 ; i < numLoop; i++) {
-	    int k = 0;
-    }
+
+  /*  printf("%s was added at %d\n", name, *(funcArgs->trackerPtr));
+
+    DIR *dirp = opendir(name);
+    if(dirp != NULL) {
+        struct dirent *dp;
+        while((dp = readdir(dirp))) { 
+            int prev = *(funcArgs->trackerPtr) - 1;
+            printf("%s will be added at %d\n", dp->d_name, *(funcArgs->trackerPtr));
+            *(funcArgs->trackerPtr) = *(funcArgs->trackerPtr) + 1; 
+        }
+    } */
+
     printf("handled: %s\n", name);
 }
 
@@ -71,14 +81,10 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    struct filenode files[100]; //stores the nodes of files/directories
-    pthread_t allThreads[100]; //stores the handles of all threads
+    
 
     int capacity = 100;
     int* capacityPTR = &capacity;
-
-
-    int tracker = 0; //tracks the total number of files/directories and threads at the same time
 
     DIR *dirp = opendir(argv[1]);
     struct dirent *dp;
@@ -86,46 +92,57 @@ int main(int argc, char** argv) {
         /*if(tracker >= capacity) {
             grow(arr, capacityPTR);
         } */
-        int prev = tracker - 1;
+        
     	if(dp->d_type == DT_REG) {
             printf("Got a file: %s\n", dp->d_name);
             
-            int totalLen = 3;
+            int totalLen = 9;
             totalLen += strlen(dp->d_name);
             char* tempName = (char *) malloc(sizeof(char *) * totalLen);
             tempName[0] = '.';
             tempName[1] = '/';
+            tempName[2] = 'A';
+            tempName[3] = 's';
+            tempName[4] = 's';
+            tempName[5] = 't';
+            tempName[6] = '0';
+            tempName[7] = '/';
             strcat(tempName, dp->d_name);
             files[tracker].name = (char *) malloc(sizeof(char *) * totalLen);
             strcpy(files[tracker].name, tempName);
             files[tracker].index = tracker;
-            pthread_create(&(files[tracker].thread), NULL, handleFile, &(files[tracker]));
-            allThreads[tracker] = files[tracker].thread;
-            printf("thread number %ld added to arr\n", files[tracker].thread);
+         //   files[tracker].trackerPtr = &tracker;
+            pthread_create(allThreads+tracker, NULL, handleFile, &(files[tracker]));
+            printf("thread number %ld added to arr\n", *(allThreads+tracker));
             tracker++;
         }
         else if(dp->d_type == DT_DIR && strcmp(".", dp->d_name) != 0 && strcmp("..", dp->d_name) != 0) {
             printf("Got a dir: %s\n", dp->d_name);
             
-            int totalLen = 3;
+            int totalLen = 9;
             totalLen += strlen(dp->d_name);
             char* tempName = (char *) malloc(sizeof(char *) * totalLen);
             tempName[0] = '.';
 		    tempName[1] = '/';
+            tempName[2] = 'A';
+            tempName[3] = 's';
+            tempName[4] = 's';
+            tempName[5] = 't';
+            tempName[6] = '0';
+            tempName[7] = '/';
 		    strcat(tempName, dp->d_name);     
 	    	files[tracker].name = (char *) malloc(sizeof(char *) * totalLen);
             strcpy(files[tracker].name, tempName);
             files[tracker].index = tracker;
-            pthread_create(&(files[tracker].thread), NULL, handleDir, &(files[tracker]));
-            allThreads[tracker] = files[tracker].thread;
-            printf("thread number %ld added to arr\n", files[tracker].thread);
+            pthread_create(allThreads+tracker, NULL, handleDir, &(files[tracker]));
+            printf("thread number %ld added to arr\n", *(allThreads+tracker));
             tracker++;
         }
     }
     int i = 0;
     for (i = 0; i < tracker; ++i) {
-        printf("Joining: %ld\n", (files[i].thread));
-        pthread_join((files[i].thread), NULL);
+        printf("Joining: %ld\n", allThreads[i]);
+        pthread_join(allThreads[i], NULL);
     }
     closedir(dirp);
     
