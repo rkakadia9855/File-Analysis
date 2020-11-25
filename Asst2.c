f47503628595ee28c8d2b6704c8117d68351becb
rkakadia9855 <rudrakakadia9855@gmail.com>
2020-11-25 12:47:25 +0530

    copied file parsing layout

--- Asst2.c
+++ Asst2.c
@@ -9,6 +9,8 @@
 #include <sys/types.h>
 #include <pthread.h>
 
+#define BUFSIZE 256
+
 pthread_t allThreads[100]; //stores the handles of all threads
 int tracker = -1; //tracks the total number of files/directories and threads at the same time
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
@@ -61,17 +63,42 @@ void* handleFile(void* kmt) {
     struct filenode * funcArgs = (struct filenode *) kmt;
     char* name = funcArgs->name;
 
-    printf("Handling: %s\n", name);
-    if(strcmp(name, "testcases.txt"))
-        numLoop = 100000000;
-    else if(strcmp(name, "readme.pdf"))
-        numLoop = 10000000;
-    else if(strcmp(name, "tokenizer.c"))
-        numLoop = 1000000;
-    for(int i = 0 ; i < numLoop; i++) {
-	    int k = 0;
+    printf("\n\n");
+    printf("inspecting %s\n", name);
+
+    int fd = open(name, O_RDONLY);
+    if (fd < 0) {
+        perror(name);
+        exit(EXIT_FAILURE);
     }
-    printf("handled: %s\n", name);
+
+    char buf[BUFSIZE];
+    int bytes, pos;
+    int ws = 0;
+
+    while (0 < (bytes = read(fd, buf, BUFSIZE))) {
+        //printf("Read %d bytes\n", bytes);
+        for (pos = 0; pos < bytes; ++pos) {
+            if (isspace(buf[pos])) {
+                ws++;
+            } else if (isalpha(buf[pos])) {
+                buf[pos] = toupper(buf[pos]);
+            } else if (buf[pos] == '\0') {
+                write(STDOUT_FILENO, "!!!", 3);
+            }
+
+        }
+        write(STDOUT_FILENO, buf, bytes);
+    }
+    // could check here whether bytes is negative
+    //  bytes == 0  - end of file
+    //  bytes <  0  - error
+
+    printf("Whitespace chars: %d\n", ws);
+
+    close(fd);
+    
+
 }
 
 void* handleDir(void* kmt) {
